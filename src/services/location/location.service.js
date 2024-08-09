3d065abff14fad319954d37e7db5dc34e6b2db81
Chris Nowak <chrisnowak7@gmail.com>
2024-08-09 04:03:59 -0500

    move location mock to local firebase function

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,18 +1,17 @@
+import { FIREBASE_PROJECT_ID } from "@env";
 import camelize from "camelize";
 
-import { locations } from "./location.mock";
-
 export const locationRequest = (searchTerm) => {
-  return new Promise((resolve, reject) => {
-    const locationMock = locations[searchTerm];
-    if (!locationMock) {
-      reject("not found");
-    }
-    resolve(locationMock);
+  console.log("my search term is " + searchTerm);
+  return fetch(
+    `http://127.0.0.1:5001/${FIREBASE_PROJECT_ID}/us-central1/geocode?city=${searchTerm}`
+  ).then((res) => {
+    return res.json();
   });
 };
 
 export const locationTransform = (result) => {
+  console.log(result);
   const formattedResponse = camelize(result);
   const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;
